f0631b6a7b36ae2c804644f9a031a7277b85ee06
takecare <rui@vazteixeira.org>
2016-09-11 19:40:01 +0100

    add episode 9 (section 2)

---
+++ s02e09/app.js
@@ -0,0 +1,10 @@
+// base/global execution context: two objects are created - 'global' and 'this'
+// on a browser, 'this' (on the global levet) points to the window object
+// (same as 'window' object), which is the global object.
+
+// both 'a' and 'b' are created at the global level here
+var a = 'Hello World!';
+
+function b() {
+    
+}
