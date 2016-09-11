bac21865e96eb366064db68a34f1409b06f0dd77
takecare <rui@vazteixeira.org>
2016-09-11 23:08:57 +0100

    add episode 11's (section 2)

--- s02e11/app.js
+++ s02e11/app.js
@@ -1,8 +1,18 @@
 b();
+
+// at this point 'a' is undefined. the interpreter has "seen" the assignment
+// and, as such, set up the variable (by reserving the memory space).
 console.log(a);
 
 var a = 'Hello World!';
 
 function b() {
     console.log('Called b!');
+}
+
+var c;
+
+if (c === undefined) {
+    console.log("never assign 'undefined' yourself");
+    console.log("otherwise it will be tricky to identify which vars haven't been assinged");
 }
