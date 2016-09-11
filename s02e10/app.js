c8639800d12a98791b71d1903801ca3491f078d9
takecare <rui@vazteixeira.org>
2016-09-11 23:00:30 +0100

    improve comments on hoisting

--- s02e10/app.js
+++ s02e10/app.js
@@ -1,11 +1,12 @@
 // even though function 'b' is not defined at this point, it's still
 // available as part of the global execution context, given our script
 // was interpreted as a whole before being executed and the function
-// is put in memory (its name and its content)
+// is put in memory (its name and its content).
 b();
 
 // the same doesn't happen with variables, as assignments only occur
-// when the code is actually run...
+// when the code is actually run... still, the variable has been setup!
+// if you remove the assignment you'll get an error!
 console.log(a);
 
 var a = 'Hello World!';
@@ -13,3 +14,5 @@ var a = 'Hello World!';
 function b() {
   console.log('Called b');
 }
+
+// this phenomenon is called hoisting.
