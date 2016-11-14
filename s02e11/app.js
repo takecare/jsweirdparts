3439875a0fb47e3fe135b34e2f1b3edfca7f1388
takecare <rui@vazteixeira.org>
2016-11-14 20:12:20 +0000

    improve explanation on undefined

--- s02e11/app.js
+++ s02e11/app.js
@@ -1,7 +1,8 @@
 b();
 
 // at this point 'a' is undefined. the interpreter has "seen" the assignment
-// and, as such, set up the variable (by reserving the memory space).
+// and, as such, set up the variable (by reserving the memory space and 
+// assinging it's value to undefined).
 console.log(a);
 
 var a = 'Hello World!';
@@ -14,5 +15,8 @@ var c;
 
 if (c === undefined) {
     console.log("never assign 'undefined' yourself");
-    console.log("otherwise it will be tricky to identify which vars haven't been assinged");
+    console.log("otherwise it will be tricky to identify which vars haven't been assigned");
 }
+
+// 'd' isn't defined at all. we'll get an error!
+console.log(d);
