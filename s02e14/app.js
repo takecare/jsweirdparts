882113c3c072ac250fa7c5cd0ac75ca3a5022457
takecare <rui@vazteixeira.org>
2016-11-14 20:37:07 +0000

    add e14 on execution context stack

--- s02e14/app.js
+++ s02e14/app.js
@@ -1,3 +1,5 @@
+// first a global execution context is created (see s02e09)...
+
 function a() {
     b();
     var c;
@@ -7,5 +9,10 @@ function b() {
     var d;
 }
 
+// ... and then a new execution context is created due to
+// the invocation of 'a'. this execution context is placed on
+// top of the execution context stack. given 'b' is invoked
+// inside 'a' yet a new execution context will be created.
 a();
 var d;
+
