502489fc0e34d039d7207cbcf8f9f92508f1cb40
takecare <rui@vazteixeira.org>
2016-11-14 19:58:40 +0000

    improve explanation of global context

--- s02e09/app.js
+++ s02e09/app.js
@@ -1,6 +1,11 @@
-// base/global execution context: two objects are created - 'global' and 'this'
-// on a browser, 'this' (on the global levet) points to the window object
-// (same as 'window' object), which is the global object.
+// execution context: when the js engine is running your code it first wraps it
+// up in what's called an execution context
+
+// base/global execution context: two objects are created - 'global' 
+// and 'this' - on a browser, 'this' (on the global level) points to the window
+// object (same as 'window' object), which is the global object.
+
+// global is anything that's not inside a function
 
 // both 'a' and 'b' are created at the global level here
 var a = 'Hello World!';
