7a5d7e45f5f4990a914cee418cbcd46ea29fd165
takecare <rui@vazteixeira.org>
2016-11-15 23:31:31 +0000

    explain dynamic typing

---
+++ s02e19/app.js
@@ -0,0 +1,10 @@
+// javascript is a dinamically typed language. this means that
+// a js engine determines the type of a variable at runtime.
+
+var a = 10;
+a = 'ten';
+a = true;
+
+if (a === true) {
+    console.log('hello');
+}
