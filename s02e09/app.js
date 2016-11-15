40395cc0c4df9f17ef962559c083f78f824a0a80
takecare <rui@vazteixeira.org>
2016-11-15 00:09:49 +0000

    add bit mentioning the reference to the outer context

--- s02e09/app.js
+++ s02e09/app.js
@@ -1,7 +1,11 @@
 // execution context: when the js engine is running your code it first wraps it
 // up in what's called an execution context
 
-// base/global execution context: two objects are created - 'global' 
+// at the base/global execution context (created for you by the js engine) you get
+//  - the 'global' object
+//  - the 'this' variable (which at the base level points to 'global')
+//  - and a link to the outer environment (this is a 'global' when inside a function
+//      on that level)
 // and 'this' - on a browser, 'this' (on the global level) points to the window
 // object (same as 'window' object), which is the global object.
 
