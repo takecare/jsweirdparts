e98b640b66afac1745f15610703fa32283e94fe5
takecare <rui@vazteixeira.org>
2016-11-17 00:29:29 +0000

    add examples of primitive types

---
+++ s03e20/app.js
@@ -0,0 +1,20 @@
+// a primitive type is a type that is not an object
+// undefined, null, boolean, number (always a floating point), string and symbol
+// symbol was introduced in es6
+
+var meaningOfLife = 42;
+console.log(typeof meaningOfLife);
+
+var notDefined;
+console.log(typeof notDefined);
+
+var empty = null;
+console.log(typeof empty);
+
+var answer = true;
+console.log(typeof answer);
+
+var name = 'Pirata'
+console.log(typeof name);
+
+// further info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
