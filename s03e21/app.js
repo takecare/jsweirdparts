d272e620400cde42607fd69129cdd4e34d8fa92d
takecare <rui@vazteixeira.org>
2016-11-20 17:21:25 +0000

    add operators examples

---
+++ s03e21/app.js
@@ -0,0 +1,7 @@
+// operators are like a special kind of funcion when you use
+// them you're using what's called: infix notation, the operator
+// is in the middle of the parameters
+
+console.log(4 + 5);
+console.log(5 / 4);
+console.log(4 == 5);
