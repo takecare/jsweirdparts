1ac9f009c29830de6af2f42227fef193d42f2483
takecare <rui@vazteixeira.org>
2016-11-23 23:58:14 +0000

    e29: checking global context for a variable

--- s02e09/app.js
+++ s02e09/app.js
@@ -1,19 +1,1 @@
-// execution context: when the js engine is running your code it first wraps it
-// up in what's called an execution context
-
-// at the base/global execution context (created for you by the js engine) you get
-//  - the 'global' object
-//  - the 'this' variable (which at the base level points to 'global')
-//  - and a link to the outer environment (this is a 'global' when inside a function
-//      on that level)
-// and 'this' - on a browser, 'this' (on the global level) points to the window
-// object (same as 'window' object), which is the global object.
-
-// global is anything that's not inside a function
-
-// both 'a' and 'b' are created at the global level here
-var a = 'Hello World!';
-
-function b() {
-    
 }
